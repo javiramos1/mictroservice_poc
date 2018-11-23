91ba90c49461058d7af21be2c371366821982c5a
Javier <javier.ramosrodriguez@irishlife.ie>
2018-11-23 13:35:50 +0000

    initial

---
+++ src/main/java/ie/irishlife/spring/boot/poc/Application.java
@@ -0,0 +1,29 @@
+package ie.irishlife.spring.boot.poc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application extends SpringBootServletInitializer {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
